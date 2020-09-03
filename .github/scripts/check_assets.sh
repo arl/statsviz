d2a5c5355b526086f0b40facd986219cde0a48ed
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-09-03 18:36:22 +0200

    ci: update check_assets.sh

--- .github/scripts/check_assets.sh
+++ .github/scripts/check_assets.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
 
+cd ${GITHUB_WORKSPACE:-$(git root)}
+
 # Re-generate assets file
-cd "${GITHUB_WORKSPACE}"
 go generate
 
 # Check that the generated assets only modifications are date-related.
 if git diff --unified=1 assets_vfsdata.go | grep -E '^(\+|\-)\s.*$' | grep -v modTime; then
   # some diff content is not date-related, failing.
-  echo "assets_vfsdata.go should be regenerated"
+  echo "assets_vfsdata.go is not up to date"
   exit 1
 fi
 
