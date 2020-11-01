13c27e08bed3f3963ac8baf8ad4c61c29957495e
Aurélien Rainone <aurelien.rainone@gmail.com>
2020-11-01 12:08:27 +0100

    ci: build examples (one per directory)

--- .github/scripts/build_examples.sh
+++ .github/scripts/build_examples.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 
-for gofile in $(find _example -name '*.go') ; do \
-  go build $gofile
+for example in $(find _example -mindepth 1 -maxdepth 1 -type d) ; do \
+  echo $example: building...
+  go build -o example.bin ./$example 
+  echo $example: success!
+  rm ./example.bin
 done
+
+go mod tidy
