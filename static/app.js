8999f306416d584a355904c3f39b0f4f94f7a898
Aurélien Rainone <476650+arl@users.noreply.github.com>
2021-04-24 17:18:33 +0200

    Issue 47/gui auto reconnect (#49)
    * Use 'for range' instead of 'for {select{}}'
    
    * static: automatic websocket reconnection (exponential backoff)
    
    fixes #47
    
    * static: cosmetics
    
    * Regenerate assets

--- static/app.js
+++ static/app.js
@@ -1,10 +1,11 @@
-(function () {
+(function() {
     function $(id) {
         return document.getElementById(id);
     }
 
     function buildWebsocketURI() {
-        var loc = window.location, ws_prot = "ws:";
+        var loc = window.location,
+            ws_prot = "ws:";
         if (loc.protocol === "https:") {
             ws_prot = "wss:";
         }
@@ -12,36 +13,47 @@
     }
 
     const dataRetentionSeconds = 60;
+    var timeout = 250;
 
-    /* WebSocket callbacks */
-
-    let socket = new WebSocket(buildWebsocketURI());
-    console.log("Attempting Connection...");
-
-    socket.onopen = () => {
-        console.log("Successfully Connected");
-    };
-
-    socket.onclose = event => {
-        console.log("Socket Closed Connection: ", event);
-        socket.send("Client Closed!")
-    };
-
-    socket.onerror = error => {
-        console.log("Socket Error: ", error);
-    };
-
-    var initDone = false;
-    socket.onmessage = event => {
-        let allStats = JSON.parse(event.data)
-        if (!initDone) {
-            stats.init(dataRetentionSeconds, allStats);
-            initDone = true;
-            return;
-        }
-
-        updateStats(allStats);
+    function clamp(val, min, max) {
+        if (val < min) return min;
+        if (val > max) return max;
+        return val;
     }
+
+    /* WebSocket connection handling */
+
+    function connect() {
+        let ws = new WebSocket(buildWebsocketURI());
+        console.log("Attempting websocket connection to statsviz server...");
+
+        ws.onopen = () => {
+            console.log("Successfully connected");
+            timeout = 250; // reset connection timeout for next time
+        };
+
+        ws.onclose = event => {
+            console.log("Closed websocket connection: ", event);
+            setTimeout(connect, clamp(timeout += timeout, 250, 5000));
+        };
+
+        ws.onerror = error => {
+            console.log("Websocket error: ", error);
+            ws.close();
+        };
+
+        var initDone = false;
+        ws.onmessage = event => {
+            let allStats = JSON.parse(event.data)
+            if (!initDone) {
+                stats.init(dataRetentionSeconds, allStats);
+                initDone = true;
+                return;
+            }
+            updateStats(allStats);
+        }
+    }
+    connect();
 
     function updateStats(allStats) {
         stats.pushData(new Date(), allStats);
