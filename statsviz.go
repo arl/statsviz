ba9daa27fef434e8783a53e879e96c9ecbd6f493
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-04-24 16:23:42 +0200

    Use 'for range' instead of 'for {select{}}'

--- statsviz.go
+++ statsviz.go
@@ -17,15 +17,17 @@ func sendStats(conn *websocket.Conn, frequency time.Duration) error {
 	tick := time.NewTicker(frequency)
 	defer tick.Stop()
 
-	var stats stats
-	for {
-		select {
-		case <-tick.C:
-			runtime.ReadMemStats(&stats.Mem)
-			stats.NumGoroutine = runtime.NumGoroutine()
-			if err := conn.WriteJSON(stats); err != nil {
-				return err
-			}
+	var (
+		stats stats
+		err   error
+	)
+	for range tick.C {
+		runtime.ReadMemStats(&stats.Mem)
+		stats.NumGoroutine = runtime.NumGoroutine()
+		if err = conn.WriteJSON(stats); err != nil {
+			break
 		}
 	}
+
+	return err
 }
