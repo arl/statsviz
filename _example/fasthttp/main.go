818b981a69816ee06a0812e61c1d1126dad6eec3
Sangbaek Lee <nupamore@gmail.com>
2021-04-26 03:27:18 +0900

    Support fasthttp (#35)
    * statsviz: conn interface, sendStats be public
    
    * _example: fasthttp, fiber
    
    * Update README.md
    
    * Revert "statsviz: conn interface, sendStats be public"
    
    This reverts commit 3cf4b7c892be352523ebbb4e7c3cf1d114b027dd.
    
    * _example: fasthttp, fiber (with cmux)
    
    * _example: fasthttp, fiber update
    
    Co-authored-by: Aurélien Rainone <476650+arl@users.noreply.github.com>

---
+++ _example/fasthttp/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/fasthttp/router"
+	"github.com/soheilhy/cmux"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
+
+	"github.com/arl/statsviz"
+	example "github.com/arl/statsviz/_example"
+)
+
+func main() {
+	// Force the GC to work to make the plots "move".
+	go example.Work()
+
+	// Create the main listener and mux
+	l, _ := net.Listen("tcp", ":8080")
+	m := cmux.New(l)
+	ws := http.NewServeMux()
+
+	// fasthttp routers
+	r := router.New()
+	r.GET("/", func(ctx *fasthttp.RequestCtx) {
+		fmt.Fprintf(ctx, "Hello, world!")
+	})
+
+	// statsviz
+	r.GET("/debug/statsviz/{filepath:*}", fasthttpadaptor.NewFastHTTPHandler(statsviz.Index))
+	ws.HandleFunc("/debug/statsviz/ws", statsviz.Ws)
+
+	// Server start
+	go http.Serve(m.Match(cmux.HTTP1HeaderField("Upgrade", "websocket")), ws)
+	go fasthttp.Serve(m.Match(cmux.Any()), r.Handler)
+	m.Serve()
+}
