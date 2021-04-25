2aa883460a9693b5b57504fe4c9d99d39e399f5a
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-04-25 13:03:03 +0200

    .github/scripts: cosmetics

--- .github/scripts/check_assets.sh
+++ .github/scripts/check_assets.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -eu
 
-cd ${GITHUB_WORKSPACE:-$(git rev-parse --show-toplevel)}
+cd "${GITHUB_WORKSPACE:-$(git rev-parse --show-toplevel)}"
 
 # Re-generate assets file
 go generate
 
 # Check that the generated assets only modifications are date-related.
-if git diff --unified=1 assets_vfsdata.go | grep -E '^(\+|\-)\s.*$' | grep -v modTime; then
+if git diff --unified=1 assets_vfsdata.go |
+  grep -E '^(\+|\-)\s.*$' |
+  grep -v modTime; then
   # some diff content is not date-related, failing.
   echo "assets_vfsdata.go is not up to date"
   exit 1
