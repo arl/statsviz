4a6762cd2c0afa02ba70345d3e16d0ddd1d0ea5b
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-04-25 20:32:43 +0200

    _example: add fasthttp+fiber dep install scripts

---
+++ _example/fasthttp/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+go get github.com/fasthttp/router
+go get github.com/soheilhy/cmux
+go get github.com/valyala/fasthttp@v1.23.0
+go get github.com/valyala/fasthttp/fasthttpadaptor@v1.23.0
