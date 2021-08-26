d38417db65228cf766822e6c5b7322ebcbc005ec
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-08-26 18:13:03 +0200

    _example: make work more predictable

--- _example/work.go
+++ _example/work.go
@@ -1,24 +1,44 @@
 package example
 
 import (
-	"math/rand"
-	"strconv"
+	"fmt"
 	"time"
 )
 
 // Work loops forever, generating a bunch of allocations of various sizes in
 // order to force the garbage collector to work.
 func Work() {
-	m := map[string][]byte{}
+	m := make(map[int]interface{})
+	i := 0
+	for ; ; i++ {
 
-	for {
-		b := make([]byte, 512+rand.Intn(16*1024))
-		m[strconv.Itoa(len(m)%(10*100))] = b
-
-		if len(m)%(10*100) == 0 {
-			m = make(map[string][]byte)
+		var obj interface{}
+		switch i % 6 {
+		case 0:
+			obj = &struct {
+				_ uint32
+				_ uint16
+			}{}
+		case 1:
+			obj = &struct {
+				_ [3]uint64
+			}{}
+		case 2:
+			obj = fmt.Sprint("a relatively long and useless string %d", i)
+		case 3:
+			obj = make([]byte, i%1024)
+		case 4:
+			obj = make([]byte, 10*i%1024)
+		case 5:
+			obj = make([]string, 512)
 		}
 
+		if i == 1000 {
+			m = make(map[int]interface{})
+			i = 0
+		}
+
+		m[i] = obj
 		time.Sleep(10 * time.Millisecond)
 	}
 }
