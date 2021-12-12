1bc81997479e7dfcfb6f398703a783e59ab20d6e
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-12-12 18:51:00 +0100

    _example: add iris
    Close #46

---
+++ _example/iris/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/kataras/iris/v12"
+
+	"github.com/arl/statsviz"
+	example "github.com/arl/statsviz/_example"
+)
+
+func main() {
+	// Force the GC to work to make the plots "move".
+	go example.Work()
+
+	app := iris.New()
+
+	// Need to run iris in a separate goroutine so we can start the dedicated
+	// http server for Statsviz.
+	go app.Listen(":8000")
+
+	mux := http.NewServeMux()
+	statsviz.Register(mux)
+	statsSrv := &http.Server{Addr: ":9000", Handler: mux}
+
+	fmt.Println("Point your browser to http://127.0.0.1:9000/debug/statsviz\n")
+
+	// NewHost puts the http server for statsviz under the control of iris but
+	// iris won't touch its handlers.
+	app.NewHost(statsSrv).ListenAndServe()
+}
