849507524d21d6aae10502aa1be7726e781766cf
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-02-18 17:10:13 +0100

    internal/static: js cosmetics

--- internal/static/buffer.js
+++ internal/static/buffer.js
@@ -5,7 +5,6 @@ var Buffer = (function() {
             if (cap - len < 0) {
                 console.Error("cap - len must be positive");
             }
-
             // TODO(arl): value using TypedArray rather than Array here
             this._buf = new Array(cap);
             this._pos = 0;
@@ -25,18 +24,17 @@ var Buffer = (function() {
                 this._buf.copyWithin(0, this._cap - this._len);
                 this._pos = this._len;
             }
-
             this._buf[this._pos] = pt;
             this._pos++;
         }
         length() {
-                if (this._pos > this._len) {
-                    return this._len;
-                }
-
-                return this._pos;
+            if (this._pos > this._len) {
+                return this._len;
             }
-            // slice returns a slice of the len latest datapoints present in the buffer.
+            return this._pos;
+        }
+
+        // slice returns a slice of the len latest datapoints present in the buffer.
         slice(len) {
             // Cap the dimension of the returned slice to the data available
             if (len > this.length()) {
@@ -47,7 +45,6 @@ var Buffer = (function() {
             return this._buf.slice(start, start + len);
         }
     }
-
 
     return Buffer;
 }());
