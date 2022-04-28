4e6a209ad7a01086943dabb61e62d6f761affd73
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-04-28 19:03:34 +0200

    Generate plot definitions from Go template

--- handlers.go
+++ handlers.go
@@ -1,7 +1,11 @@
 package statsviz
 
 import (
+	"bytes"
+	_ "embed"
+	"html/template"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -20,7 +24,34 @@ var Index = IndexAtRoot(defaultRoot)
 // path that is different than /debug/statsviz.
 func IndexAtRoot(root string) http.HandlerFunc {
 	prefix := strings.TrimRight(root, "/") + "/"
-	return http.StripPrefix(prefix, http.FileServer(http.FS(static.Assets))).ServeHTTP
+	assetsFS := http.FileServer(http.FS(static.Assets))
+	return http.StripPrefix(prefix, hijack(assetsFS)).ServeHTTP
+}
+
+var (
+	//go:embed plotsdef.tpl
+	plotsdef    string
+	plotsdefTpl = template.Must(template.New("plotsdef").Parse(plotsdef))
+)
+
+// hijack returns a handler that forward all requests to that do not target
+// plotsdef.js. Requests targeting plotsdef.js respond with the generated plots
+// definition.
+func hijack(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "plotsdef.js" {
+			buf := bytes.Buffer{}
+			if err := plotsdefTpl.Execute(&buf, nil); err != nil {
+				panic("template execute :" + err.Error())
+			}
+
+			w.Header().Add("Content-Length", strconv.Itoa(buf.Len()))
+			w.Header().Add("Content-Type", "text/javascript; charset=utf-8")
+			buf.WriteTo(w)
+			return
+		}
+		h.ServeHTTP(w, r)
+	}
 }
 
 // Ws is a default Websocket handler, created with NewWsHandler, sending statistics
