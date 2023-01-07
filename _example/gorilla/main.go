b5f9dd6170f05f702ae2b0ee08059dd1dfc664d9
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-01-07 21:00:49 +0100

    _example/gorilla: switch to statsviz.Endpoint

--- _example/gorilla/main.go
+++ _example/gorilla/main.go
@@ -14,10 +14,13 @@ func main() {
 	// Force the GC to work to make the plots "move".
 	go example.Work()
 
+	// Create statsviz endpoint.
+	se := statsviz.NewEndpoint()
+
 	// Create a Gorilla router and register statsviz handlers.
 	r := mux.NewRouter()
-	r.Methods("GET").Path("/debug/statsviz/ws").Name("GET /debug/statsviz/ws").HandlerFunc(statsviz.Ws)
-	r.Methods("GET").PathPrefix("/debug/statsviz/").Name("GET /debug/statsviz/").Handler(statsviz.Index)
+	r.Methods("GET").Path("/debug/statsviz/ws").Name("GET /debug/statsviz/ws").HandlerFunc(se.Ws())
+	r.Methods("GET").PathPrefix("/debug/statsviz/").Name("GET /debug/statsviz/").Handler(se.Index())
 
 	mux := http.NewServeMux()
 	mux.Handle("/", r)
