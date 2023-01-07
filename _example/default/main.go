6a7723612741941403c2ef5948b71772b13dc441
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-01-07 13:20:06 +0100

    _example/default: switch to statsviz.Endpoint

--- _example/default/main.go
+++ _example/default/main.go
@@ -13,8 +13,11 @@ func main() {
 	// Force the GC to work to make the plots "move".
 	go example.Work()
 
-	// Register statsviz handlers on the default serve mux.
-	statsviz.RegisterDefault()
+	// Create statsviz se.
+	se := statsviz.NewEndpoint()
+
+	// Register statsviz on the default serve mux.
+	se.Register(http.DefaultServeMux)
 
 	fmt.Println("Point your browser to http://localhost:8080/debug/statsviz/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
