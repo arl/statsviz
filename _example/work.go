a1709d9f187296cf833003ac83aa6f159a78c2a2
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-02-28 19:22:52 +0100

    _example: simpler work function

--- _example/work.go
+++ _example/work.go
@@ -1,70 +1,39 @@
 package example
 
 import (
-	"context"
-	"fmt"
 	"math/rand"
+	"reflect"
+	"strconv"
 	"time"
 )
 
-// Work loops forever, generating a bunch of allocations of various sizes, plus
-// some goroutines, in order to force the garbage collector to work.
+// Wwork loops forever, generating allocations of various sizes, in order to
+// create artificial work for a nice 'demo effect'.
 func Work() {
-	work(context.Background(), 0)
+	m := make(map[int64]interface{})
+	tick := time.NewTicker(30 * time.Millisecond)
+	clearTick := time.NewTicker(1 * time.Second)
+
+	for {
+		select {
+		case <-clearTick.C:
+			m = make(map[int64]interface{})
+		case ts := <-tick.C:
+			m[ts.UnixNano()] = newStruct()
+		}
+	}
 }
 
-func work(ctx context.Context, lvl int) {
-	m := make(map[int]interface{})
-	tick := time.NewTicker(10 * time.Millisecond)
-
-	for i := 0; ; i++ {
-		select {
-		case <-ctx.Done():
-			return
-		case <-tick.C:
-			break
-		}
-
-		var obj interface{}
-
-		switch rand.Intn(10) % 10 {
-		case 0:
-			obj = &struct {
-				_ uint32
-				_ uint16
-			}{}
-		case 1:
-			obj = &struct {
-				_ [3]uint64
-			}{}
-		case 2:
-			obj = fmt.Sprintf("a relatively long and useless string %d", i)
-		case 3:
-			obj = make([]byte, i%1024)
-		case 4, 5:
-			obj = make([]byte, 10*i%1024)
-		case 6, 7:
-			obj = make([]string, 512)
-			go func() {
-				time.Sleep(time.Second)
-			}()
-		case 8:
-			obj = make([]byte, 16*1024)
-		case 9:
-			if lvl > 2 {
-				break
-			}
-			// Sometimes start another goroutine, just because...
-			ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
-			defer cancel()
-			work(ctx, lvl+1)
-		}
-
-		if i == 1000 {
-			m = make(map[int]interface{})
-			i = 0
-		}
-
-		m[i] = obj
+// create a randomly-sized struct (for size classes plot).
+func newStruct() interface{} {
+	nfields := rand.Intn(32)
+	var fields []reflect.StructField
+	for i := 0; i < nfields; i++ {
+		fields = append(fields, reflect.StructField{
+			Name:    "f" + strconv.Itoa(i),
+			PkgPath: "main",
+			Type:    reflect.TypeOf(""),
+		})
 	}
+	return reflect.New(reflect.StructOf(fields)).Interface()
 }
