d6efe46da7b9d21cfe8df8c56cff8199b47b0e2e
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-06-28 21:00:13 +0200

    Package go doc fixes

--- statsviz.go
+++ statsviz.go
@@ -1,20 +1,20 @@
 // Package statsviz allows to visualise Go program runtime metrics data in real
 // time: heap, objects, goroutines, GC pauses, scheduler, etc. in your browser.
 //
-// Create a Statsviz [Endpoint] and register it with your server [http.ServeMux]
+// Create an [Endpoint] and register it with your server [http.ServeMux]
 // (preferred method):
 //
 //	mux := http.NewServeMux()
 //	endpoint := statviz.NewEndpoint()
 //	endpoint.Register(mux)
 //
-// Or register with [http.DefaultServeMux`]:
+// Or register with [http.DefaultServeMux]:
 //
 //	endpoint := statviz.NewEndpoint()
 //	endpoint.Register(http.DefaultServeMux)
 //
-// By default Statsviz is served at `/debug/statsviz/`. You can change that (and
-// other things) using methods on the [statsviz.Endpoint] instance.
+// By default Statsviz is served at `/debug/statsviz/`. You can change that, as well
+// as configure other things, using methods of an [Endpoint].
 //
 // If your application is not already running an HTTP server, you need to start
 // one. Add "net/http" and "log" to your imports and the following code to your
@@ -179,7 +179,7 @@ func (e *Endpoint) Ws() http.HandlerFunc {
 	}
 }
 
-// sendStats indefinitely send runtime statistics on the websocket connection.
+// sendStats sends runtime statistics on the websocket connection.
 func (e *Endpoint) sendStats(conn *websocket.Conn, frequency time.Duration) error {
 	tick := time.NewTicker(frequency)
 	defer tick.Stop()
