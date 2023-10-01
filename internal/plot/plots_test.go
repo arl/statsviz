4dcedd9b092ee858e8d2b6798d868018f5302a55
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-10-01 15:42:48 +0200

    internal/plot: disregard /godebug metrics in TestUnusedRuntimeMetrics

--- internal/plot/plots_test.go
+++ internal/plot/plots_test.go
@@ -3,14 +3,15 @@ package plot
 import (
 	"fmt"
 	"runtime/metrics"
+	"strings"
 	"testing"
 )
 
 func TestUnusedRuntimeMetrics(t *testing.T) {
-	// This test checks if all runtime metrics are plotted. It can fail, it's
-	// just informative.
+	// This "test" can't fail. It just prints which of the metrics exported by
+	// /runtime/metrics are not used in any Statsviz plot.
 	for _, m := range metrics.All() {
-		if _, ok := usedMetrics[m.Name]; !ok {
+		if _, ok := usedMetrics[m.Name]; !ok && !strings.HasPrefix(m.Name, "/godebug/") {
 			fmt.Printf("runtime/metric %q is not used\n", m.Name)
 		}
 	}
