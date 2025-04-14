c689955d9dfd1c3414e5a7639cb93ce1f3d66446
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-04-14 21:21:23 +0200

    web: use relative paths

--- internal/web/vite.config.js
+++ internal/web/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [svelte()],
-})
+});
