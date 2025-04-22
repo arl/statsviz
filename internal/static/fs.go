a5e5f3495a167e344a5f77d4e1d273d504a35a51
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-04-22 11:54:22 +0200

    static: zip embedded assets
    This trims ~3MB from final binary size. Assets (dist directory) is
    zipped into dist.zip, which is embedded and unzipped once, at runtime.

--- internal/static/fs.go
+++ internal/static/fs.go
@@ -1,18 +1,23 @@
 package static
 
 import (
-	"embed"
+	"archive/zip"
+	"bytes"
+	_ "embed"
 	"fmt"
 	"io/fs"
 	"sync"
 )
 
-//go:embed dist
-var assets embed.FS
+//go:embed dist.zip
+var distzip []byte
 
 var Assets = sync.OnceValue(func() fs.FS {
-	var err error
-	webFS, err := fs.Sub(assets, "dist")
+	zr, err := zip.NewReader(bytes.NewReader(distzip), int64(len(distzip)))
+	if err != nil {
+		panic(fmt.Sprintf("error reading dist.zip: %s", err))
+	}
+	webFS, err := fs.Sub(zr, "dist")
 	if err != nil {
 		panic(fmt.Sprintf("error loading frontend assets: %s", err))
 	}
--- internal/static/src/main.js
+++ internal/static/src/main.js
@@ -1,7 +1,5 @@
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "@fortawesome/fontawesome-free";
 
 import "./style.css";
 import { startApp } from "./app.js";
