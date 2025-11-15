3ae2a2c7d4d920741a07f1c5a9d11ab32748200a
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-11-15 12:36:38 +0100

    internal/plot: rewrite TestUnusedRuntimeMetrics

--- internal/plot/plots_test.go
+++ internal/plot/plots_test.go
@@ -2,12 +2,46 @@ package plot
 
 import (
 	"fmt"
+	"maps"
 	"os"
 	"runtime/metrics"
+	"slices"
 	"strings"
 	"testing"
 	"text/tabwriter"
 )
+
+func TestUnusedRuntimeMetrics(t *testing.T) {
+	// This test just prints the metrics we're not using in any plot. It can't
+	// fail, it's informational.
+	unused := maps.Clone(reg().allnames)
+
+	reg := reg()
+	for _, d := range reg.descriptions {
+		for _, m := range d.metrics {
+			delete(unused, m)
+		}
+	}
+
+	// remove godebug metrics
+	for m := range unused {
+		if strings.HasPrefix(m, "/godebug/") {
+			delete(unused, m)
+		}
+	}
+
+	if len(unused) == 0 {
+		return
+	}
+	t.Log("some runtime metrics are not used by any plot:\n")
+	all := metrics.All()
+	w := tabwriter.NewWriter(os.Stderr, 0, 8, 2, ' ', 0)
+	for m := range unused {
+		desc := all[slices.IndexFunc(all, func(desc metrics.Description) bool { return desc.Name == m })]
+		fmt.Fprintf(w, "\t%s\t%s\t%s\n", desc.Name, kindstr(desc.Kind), clampstr(desc.Description))
+	}
+	w.Flush()
+}
 
 func kindstr(k metrics.ValueKind) string {
 	switch k {
@@ -22,23 +56,10 @@ func kindstr(k metrics.ValueKind) string {
 	}
 }
 
-func TestUnusedRuntimeMetrics(t *testing.T) {
-	// Creating a config compiles the list of metrics used by Statsviz.
-	l, err := NewList(nil)
-	if err != nil {
-		t.Fatal(err)
+func clampstr(s string) string {
+	const maxlen = 80
+	if len(s) > maxlen {
+		return s[:maxlen-3] + "..."
 	}
-	_ = l.Config()
-
-	// This "test" can't fail. It just prints which of the metrics exported by
-	// runtime/metrics are not used in any Statsviz plot.
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-	for _, m := range metrics.All() {
-		if _, ok := l.usedMetrics[m.Name]; !ok &&
-			!strings.HasPrefix(m.Name, "/godebug/") &&
-			m.Name != "/gc/pauses:seconds" /* deprecated name */ {
-			fmt.Fprintf(w, "%s\t%s\n", m.Name, kindstr(m.Kind))
-		}
-	}
-	w.Flush()
+	return s
 }
